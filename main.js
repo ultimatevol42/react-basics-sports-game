2d438296e85445d1badb17e9739bcdce368791aa
Christopher Haley <ultimatevol42@gmail.com>
2020-08-27 11:54:17 -0500

    new

--- main.js
+++ main.js
@@ -1,124 +1,180 @@
-function App(props) {
-    return <Game venue='Appropriate Name Stadium' />
+function Team(props) {
+  let shotPercentageDiv;
+
+  if (props.stats.shots) {
+    const shotPercentage = Math.round(
+      (props.stats.score / 2 / props.stats.shots) * 100
+    );
+
+    shotPercentageDiv = (
+      <div>
+        <strong>Shooting %: {shotPercentage}</strong>
+      </div>
+    );
+  }
+
+  return (
+    <div className="Team">
+      <h2>{props.name}</h2>
+
+      <div className="identity">
+        <img src={props.logo} alt={props.name} />
+      </div>
+
+      <div>
+        <strong>Shots:</strong> {props.stats.shots}
+      </div>
+
+      <div>
+        <strong>Score:</strong> {props.stats.score}
+      </div>
+
+      {shotPercentageDiv}
+
+      <button onClick={props.shotHandler}>Shoot!</button>
+    </div>
+  );
 }
 
-class Team extends React.Component {
-    constructor(props) {
-        super(props)
-        this.handleShot = this.handleShot.bind(this)
-    }
+function ScoreBoard(props) {
+  return (
+    <div className="ScoreBoard">
+      <div className="teamStats">
+        <h3>VISITORS</h3>
+        <h3>{props.visitingTeamStats.score}</h3>
+      </div>
 
-    handleShot() {
-        const shootAttempt = new Audio("media/audio/woodbat.mp3")
-        const scorePoint = new Audio("media/audio/Ball+Hit+Cheer.mp3")
-        let score = this.props.score
-        let shots = this.props.shots
+      <h3>SCOREBOARD</h3>
 
-        if ((Math.random() * 10) > 6) {
-            score++
-            shots++
-            scorePoint.play()
-        }
-        else {
-            shots++
-            shootAttempt.play()
-        }
-        this.props.handleGame(score, shots)
-    }
-
-    render() {
-        const ratio = (this.props.score / this.props.shots) || 0
-        return (
-            <div className={this.props.teamOrigin}>
-                <Scoreboard
-                    score={this.props.score}
-                    team={this.props.teamOrigin}
-                />
-                <h2>Team {this.props.name}</h2>
-                <img src={this.props.logo} />
-                <p>Score: {this.props.score}</p>
-                <p>Shots Taken: {this.props.shots}</p>
-                <p style={{ visibility: this.props.visibility }}>Shot Ratio: {(ratio * 100).toFixed(3) + '%'}</p>
-                <button onClick={this.handleShot}>Shoot!</button>
-            </div>
-        )
-    }
+      <div className="teamStats">
+        <h3>HOME</h3>
+        <h3>{props.homeTeamStats.score}</h3>
+      </div>
+    </div>
+  );
 }
 
 class Game extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            homeScore: 0,
-            homeShots: 0,
-            homeVis: "hidden",
+  constructor(props) {
+    super(props);
 
-            visitScore: 0,
-            visitShots: 0,
-            visitVis: "hidden"
-        }
-        this.handleHome = this.handleHome.bind(this)
-        this.handleVisit = this.handleVisit.bind(this)
-        this.handleReset = this.handleReset.bind(this)
-    }
-    handleHome(score, shots) {
-        this.setState({ homeScore: score, homeShots: shots, homeVis: "visible" })
+    this.state = {
+      resetCount: 0,
+      homeTeamStats: {
+        shots: 0,
+        score: 0,
+      },
+      visitingTeamStats: {
+        shots: 0,
+        score: 0,
+      },
+    };
+
+    this.shotSound = new Audio("./audio/Ball+hit+Cheer.mpc");
+    this.scoreSound = new Audio("./audio/woodbat.mps");
+  }
+
+  shoot = (team) => {
+    const teamStatsKey = `${team}TeamStats`;
+    let score = this.state[teamStatsKey].score;
+    this.shotSound.play();
+
+    if (Math.random() > 0.5) {
+      score += 1;
+
+      setTimeout(() => {
+        this.scoreSound.play();
+      }, 100);
     }
 
-    handleVisit(score, shots) {
-        this.setState({ visitScore: score, visitShots: shots, visitVis: "visible" })
-    }
+    this.setState((state, props) => ({
+      [teamStatsKey]: {
+        shots: state[teamStatsKey].shots + 1,
+        score,
+      },
+    }));
+  };
 
-    handleReset() {
-        if(this.state.homeShots !== 0 || this.state.visitShots !== 0)
-            {alert("Game reset!")}
-        this.setState({
-            homeScore: 0, homeShots: 0, homeVis: "hidden",
-            visitScore: 0, visitShots: 0, visitVis: "hidden"
-        })
-    }
+  resetGame = () => {
+    this.setState((state, props) => ({
+      resetCount: state.resetCount + 1,
+      homeTeamStats: {
+        shots: 0,
+        score: 0,
+      },
+      visitingTeamStats: {
+        shots: 0,
+        score: 0,
+      },
+    }));
+  };
 
-    render() {
-        return (
-            <>
-                <button onClick={this.handleReset}>Reset Game</button>
-                <br />
-                <h2>Welcome to {this.props.venue}!</h2>
-                <div id="container">
-                    <Team
-                        teamOrigin="Home"
-                        name="Boston Red Sox"
-                        log="https://upload.wikimedia.org/wikipedia/en/thumb/6/6d/RedSoxPrimary_HangingSocks.svg/1200px-RedSoxPrimary_HangingSocks.svg.png"
-                        handleGame={this.handleVisit}
-                        score={this.state.visitScore}
-                        shots={this.state.visitShots}
-                        visibility={this.state.visitVis}
-                    />
-                    <Team
-                        teamOrigin="Visiting"
-                        name="NY Yankees"
-                        logo="https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/NewYorkYankees_caplogo.svg/800px-NewYorkYankees_caplogo.svg.png"
-                        handleGame={this.handleHome}
-                        score={this.state.homeScore}
-                        shots={this.state.homeShots}
-                        visibility={this.state.homeVis}
-                    />
-                </div>
-            </>
-        )
-    }
-}
-
-const Scoreboard = (props) => {
+  render() {
     return (
-        <>
-            <h2>{props.team} Score</h2>
-            <h2>{props.score}</h2>
-        </>
-    )
+      <div className="Game">
+        <ScoreBoard
+          visitingTeamStats={this.state.visitingTeamStats}
+          homeTeamStats={this.state.homeTeamStats}
+        />
+
+        <h1>Welcome to {this.props.venue}</h1>
+        <div className="stats">
+          <Team
+            name={this.props.visitingTeam.name}
+            logo={this.props.visitingTeam.logoSrc}
+            stats={this.state.visitingTeamStats}
+            shotHandler={() => this.shoot("visiting")}
+          />
+
+          <div className="versus">
+            <h1>VS</h1>
+            <div>
+              <strong>Resets:</strong> {this.state.resetCount}
+              <button onClick={this.resetGame}>Reset Game</button>
+            </div>
+          </div>
+
+          <Team
+            name={this.props.homeTeam.name}
+            logo={this.props.homeTeam.logoSrc}
+            stats={this.state.homeTeamStats}
+            shotHandler={() => this.shoot("home")}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-)
+function App(props) {
+  const Cloud = {
+    name: "Cloud",
+    logoSrc: "./images/Cloud.jpg",
+  };
+
+  const Tifa = {
+    name: "Tifa",
+    logoSrc: "./images/Tifa.jpg",
+  };
+
+  const Cid = {
+    name: "Cid",
+    logoSrc: "./images/Cid.jpg",
+  };
+
+  const Barrett = {
+    name: "Barrett",
+    logoSrc: "./images/Barrett.jpg",
+  };
+
+  return (
+    <div className="App">
+      <Game venue="Midgar Arena" homeTeam={Cloud} visitingTeam={Barrett} />
+      <Game venue="Gaia Arena" homeTeam={Cid} visitingTeam={Tifa} />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// made to look and act like Davy's  code \\
